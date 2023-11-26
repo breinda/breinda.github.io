a1b90a397b00cf8b0a4a6fb39b051de6a6d96280
Brenda C <brendac@live.it>
2023-11-25 23:44:52 -0300

    Use local storage to remember prior set style

---
+++ scripts/styleSetter.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const TRUE_STR = 'true'
+const FALSE_STR = 'false'
+
+const THEME_STR = 'theme'
+const DARK_MODE = 'dark-mode'
+const LIGHT_MODE = 'light-mode'
+
+const DISPLAY_NONE_STR = 'none'
+const DISPLAY_BLOCK_STR = 'block'
+
+const styleswitcher = document.getElementById('styleswitcher')
+
+if (!localStorage.getItem(THEME_STR)) {
+	console.log('no theme defined => populating theme as LIGHT (default)')
+
+	populateStorage()
+} else {
+	console.log('theme already defined => setting style according to theme')
+	setStyles()
+}
+
+function switchDarkLightTheme() {
+	console.log('switching between light/dark...')
+
+	let body = document.body
+	body.classList.toggle('dark-mode')
+	populateStorage()
+}
+
+// populate local storage with values, then apply those values
+// default mode is LIGHT MODE
+function populateStorage() {
+	const body = document.body
+	const isDarkModeToggled = body.classList.contains('dark-mode')
+
+	if (isDarkModeToggled) {
+		console.log('dark mode is toggled => setting style as DARK')
+		localStorage.setItem(THEME_STR, DARK_MODE)
+	} else {
+		console.log('light mode is toggled => setting style as LIGHT')
+		localStorage.setItem(THEME_STR, LIGHT_MODE)
+	}
+
+	setStyles()
+}
+
+// apply style that is set in local storage
+function setStyles() {
+	const theme = localStorage.getItem(THEME_STR)
+
+	const body = document.body
+
+	const moonSvg = document.getElementById('moon-svg')
+	const sunSvg = document.getElementById('sun-svg')
+
+	// const moonSvgDisplay = window.getComputedStyle(moonSvg).getPropertyValue('display')
+	// const sunSvgDisplay = window.getComputedStyle(sunSvg).getPropertyValue('display')
+
+	if (theme === LIGHT_MODE) {
+		// light mode
+		console.log('theme is LIGHT -> hiding sun icon, displaying moon icon')
+
+		sunSvg.style.display = DISPLAY_NONE_STR
+		moonSvg.style.display = DISPLAY_BLOCK_STR
+
+		body.classList.remove(DARK_MODE)
+	} else {
+		// dark mode
+		console.log('theme is DARK -> hiding moon icon, displaying sun icon')
+
+		sunSvg.style.display = DISPLAY_BLOCK_STR
+		moonSvg.style.display = DISPLAY_NONE_STR
+
+		body.classList.add(DARK_MODE)
+	}
+}
+
+styleswitcher.onclick = switchDarkLightTheme
