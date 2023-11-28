9dc5f9c4a9c11ab5cbb05556b36c5827a55bb5e3
Brenda C <brendac@live.it>
2023-11-27 21:08:57 -0300

    Standardise some variable names

--- scripts/styleSetter.js
+++ scripts/styleSetter.js
@@ -4,8 +4,8 @@ const TRUE_STR = 'true'
 const FALSE_STR = 'false'
 
 const THEME_STR = 'theme'
-const DARK_MODE = 'dark-mode'
-const LIGHT_MODE = 'light-mode'
+const DARK_MODE_STR = 'dark-mode'
+const LIGHT_MODE_STR = 'light-mode'
 
 const DISPLAY_NONE_STR = 'none'
 const DISPLAY_BLOCK_STR = 'block'
@@ -25,7 +25,7 @@ function switchDarkLightTheme() {
 	console.log('switching between light/dark...')
 
 	let body = document.body
-	body.classList.toggle('dark-mode')
+	body.classList.toggle(DARK_MODE_STR)
 	populateStorage()
 }
 
@@ -33,14 +33,14 @@ function switchDarkLightTheme() {
 // default mode is LIGHT MODE
 function populateStorage() {
 	const body = document.body
-	const isDarkModeToggled = body.classList.contains('dark-mode')
+	const isDarkModeToggled = body.classList.contains(DARK_MODE_STR)
 
 	if (isDarkModeToggled) {
 		console.log('dark mode is toggled => setting style as DARK')
-		localStorage.setItem(THEME_STR, DARK_MODE)
+		localStorage.setItem(THEME_STR, DARK_MODE_STR)
 	} else {
 		console.log('light mode is toggled => setting style as LIGHT')
-		localStorage.setItem(THEME_STR, LIGHT_MODE)
+		localStorage.setItem(THEME_STR, LIGHT_MODE_STR)
 	}
 
 	setStyles()
@@ -58,14 +58,14 @@ function setStyles() {
 	// const moonSvgDisplay = window.getComputedStyle(moonSvg).getPropertyValue('display')
 	// const sunSvgDisplay = window.getComputedStyle(sunSvg).getPropertyValue('display')
 
-	if (theme === LIGHT_MODE) {
+	if (theme === LIGHT_MODE_STR) {
 		// light mode
 		console.log('theme is LIGHT -> hiding sun icon, displaying moon icon')
 
 		sunSvg.style.display = DISPLAY_NONE_STR
 		moonSvg.style.display = DISPLAY_BLOCK_STR
 
-		body.classList.remove(DARK_MODE)
+		body.classList.remove(DARK_MODE_STR)
 	} else {
 		// dark mode
 		console.log('theme is DARK -> hiding moon icon, displaying sun icon')
@@ -73,7 +73,7 @@ function setStyles() {
 		sunSvg.style.display = DISPLAY_BLOCK_STR
 		moonSvg.style.display = DISPLAY_NONE_STR
 
-		body.classList.add(DARK_MODE)
+		body.classList.add(DARK_MODE_STR)
 	}
 }
 
